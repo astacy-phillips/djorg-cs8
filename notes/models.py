d1e8487299e03c26f1c153f7e93ad2f392c4c56a
Amanda Phillips <amanda@phillipsdev.org>
2018-06-26 12:55:27 -0500

    Add categories and personal note

--- notes/models.py
+++ notes/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 
 
 class Note (models.Model):
-    # TODO: Add user/autho who created it
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
+    category = models.CharField(max_length=20)
     # TODO: Tagging system or categories
+
+
+class PersonalNote (Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Author
